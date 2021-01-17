f9342a9fc7d6cb89a0ff2d1ac58510439c4244fb
Huangyuren <forautodesk1@gmail.com>
2021-01-17 19:29:49 +0800

    [Add] LeetCode 142, magical fast & slow pointer.

---
+++ linkedList_cycle2.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==nullptr) return nullptr;
+        ListNode* fastptr = head->next;
+        ListNode* slowptr = head;
+        // Finding loop is posible
+        while(fastptr != slowptr){
+            if(fastptr==nullptr || slowptr==nullptr || fastptr->next==nullptr){
+                return nullptr;
+            }
+            fastptr = fastptr->next->next;
+            slowptr = slowptr->next;
+        }
+        //Ok, no loop, then check at which point will these two pointers meet.
+        fastptr = fastptr->next;
+        slowptr = head;
+        while(fastptr != slowptr){
+            fastptr = fastptr->next;
+            slowptr = slowptr->next;
+        }
+        return fastptr;
+    }
+};
