a10e86c79106cd000f5e8daa9dbe86e6c0acca56
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-01-25 23:21:09 +0800

    [Add] LeetCode 46, using swapping to permute.
    great thinking

---
+++ Permutations.cpp
@@ -0,0 +1,21 @@
+// Solution using index moving and gradually swapped elements for all possibilities.
+// I think it is a little bit hard to figure out.
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        solver(nums, 0, ans);
+        return ans;
+    }
+    void solver(vector<int>&nums, int start, vector<vector<int>>& ans){
+        if(start >= nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=start; i<nums.size(); i++){
+            swap(nums[start], nums[i]);
+            solver(nums, start+1, ans);
+            swap(nums[start], nums[i]);
+        }
+    }
+};
