0ba6366c796ce4cd0db598b80a9fd35c8dd5c9f6
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-01-25 15:40:01 +0800

    [Add] LeetCode 138, deepcopy of linked list.
    Achieve deepcopy by means of augmenting original list by 2x, then cropping out these augmented elements out for return, nice concept.

---
+++ CopyListwithRandomPointer.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == nullptr) return nullptr;
+        Node* dummy = head;
+        // append extra duplicated node right before original node.
+        while(dummy){
+            Node* backup = dummy->next;
+            Node* tmp = new Node(dummy->val);
+            dummy->next = tmp;
+            tmp->next = backup;
+            dummy = dummy->next->next;
+        }
+        // processing random pointer
+        dummy = head;
+        while(dummy){
+            if(dummy->random){
+                dummy->next->random = dummy->random->next;
+            }
+            dummy = dummy->next->next;
+        }
+        // restore original list & crop out our answer
+        dummy = head;
+        Node* dummyhead = new Node(0);
+        Node* res = dummyhead;
+        while(dummy){
+            dummyhead->next = dummy->next;
+            dummy->next = dummy->next->next;
+            dummyhead = dummyhead->next;
+            dummy = dummy->next;
+        }
+        return res->next;
+    }
+};
