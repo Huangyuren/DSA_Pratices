2b6d7208b30cd62eb966a15b4835b7eac1a09780
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-01-28 00:42:36 +0800

    [Add] LeetCode 139, using dp for word segmentation.

---
+++ WordBreak.cpp
@@ -0,0 +1,30 @@
+// DP solution
+// The idea is that we will gradually segment out all words
+// exist both in wordDict & s.
+// For example, s = "garbages", wordDict={"garbage", "garb", "ages"}
+// Resultant dp would be like: {true, false, false, false, true, false, false, true, true}
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        if(wordDict.size()==0) return false;
+        if(s.size()==0) return true;
+        unordered_map<string, int> mymap;
+        for(int i=0; i<wordDict.size(); i++){
+            mymap.emplace(wordDict[i], 1);
+        }
+        vector<bool> dp(s.size()+1, false);
+        dp[0] = true;
+        for(int i=1; i<=s.size(); i++){
+            for(int j=i-1; j>=0; j--){
+                if(dp[j]){
+                    string tmp = s.substr(j, i-j);
+                    if(mymap.find(tmp) != mymap.end()){
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
