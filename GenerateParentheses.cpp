f741f2777da2a33936d42bcbba13118c74edfd61
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-02-01 01:50:18 +0800

    [Add] LeetCode 22, logical thinking, recursion

---
+++ GenerateParentheses.cpp
@@ -0,0 +1,25 @@
+// Point: left bracket <= n, and right bracket == left
+// Recursive & logical thinking
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        string currstr="";
+        solver(res, currstr, 0, 0, n);
+        return res;
+    }
+    void solver(vector<string>& res, string currstr, int leftcount, int rightcount, int lim){
+        // printf("Left count: %d, right count: %d, currstr: %s\n", leftcount, rightcount, currstr.c_str());
+        if(leftcount == lim && rightcount == lim){
+            res.push_back(currstr);
+            return;
+        }
+        if(leftcount < lim){
+            solver(res, currstr+'(', leftcount+1, rightcount, lim);
+        }
+        if(rightcount < leftcount){
+            solver(res, currstr+')', leftcount, rightcount+1, lim);
+        }
+        return;
+    }
+};
