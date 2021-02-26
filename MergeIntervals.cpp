46a59b2f51d5dc939a0138303760d5869bad32de
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-02-26 21:38:21 +0800

    Update MergeIntervals.cpp

--- MergeIntervals.cpp
+++ MergeIntervals.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if(intervals.size()==0) return {};
         // sort(intervals.begin(), intervals.end(), [](auto a, auto b) -> bool{ return a[0]<b[0];});
         // sorting will take intervals[i][0] as default values being compared.
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        vector<int> *prev = &ans.back();
-        vector<int> *curr;
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+        vector<int>* prev = &res.back();
         for(int i=1; i<intervals.size(); i++){
-            curr = &intervals[i];
-            if(prev->at(1) >= curr->at(0)){
-                prev->at(1) = max(prev->at(1), curr->at(1));
+            if(prev->at(1) < intervals[i][0]){
+                res.push_back(intervals[i]);
+                prev = &res.back();
             }
             else
             {
-                ans.push_back(intervals[i]);
-                prev = &ans.back();
+                prev->at(1) = max(prev->at(1), intervals[i][1]);
             }
         }
-        return ans;
-//         Slower but clearer concepts
+        return res;
+//         In clearer concepts
 //         sort(intervals.begin(), intervals.end(), [](vector<int>a, vector<int>b) -> bool{return a[0]<b[0];});
 //         vector<vector<int>> res;
 //         res.push_back(intervals[0]);
