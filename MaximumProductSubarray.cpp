b6cb964e1e8605b657acb2c9d5efa10c2888064d
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-02-27 16:30:40 +0800

    Update MaximumProductSubarray.cpp

--- MaximumProductSubarray.cpp
+++ MaximumProductSubarray.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         if(nums.empty()) return 0;
-        int maxsofar = 1; // For positive value
-        int minsofar = 1; // For negative value
-        int res = INT_MIN;
+        int maxsofar = 1; // For positive value, and should be careful, initialized by 1
+        int minsofar = 1; // For negative value, and should be careful, initialized by 1
+        int res = INT_MIN; // Need additional res for return, not returning maxsofar
         for(int i=0; i<nums.size(); i++){
             if(nums[i] < 0){
                 swap(maxsofar, minsofar);
             }
-            maxsofar = max(nums[i], maxsofar * nums[i]); // should take current into consideration, in case of 0
-            minsofar = min(nums[i], minsofar * nums[i]); // should take current into consideration, in case of 0
+            maxsofar = max(nums[i], maxsofar * nums[i]); // should take current value into consideration, in case of 0
+            minsofar = min(nums[i], minsofar * nums[i]); // should take current value into consideration, in case of 0
             res = max(res, maxsofar);
         }
         return res;
