e95de250a881718af4ac64ecad154b8777be53bb
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-03-05 00:50:52 +0800

    Update FindDuplicateNumber.cpp

--- FindDuplicateNumber.cpp
+++ FindDuplicateNumber.cpp
@@ -3,10 +3,10 @@ public:
     int findDuplicate(vector<int>& nums) {
         if(nums.size() == 0) return 0;
         for(int i=0; i<nums.size(); i++){
-            int indextrue = abs(nums[i]) - 1;
+            int indextrue = abs(nums[i]) - 1; // if with same nums value, will have same currindex
             // Negative sign means that this element
             // has already been traversed, so ans is it.
-            if(nums[indextrue] < 0){
+            if(nums[indextrue] < 0){ // So by checking sign can determine visited or not
                 return abs(nums[i]);
             }
             else
@@ -14,6 +14,6 @@ public:
                 nums[indextrue] *= -1;
             }
         }
-        return 0;
+        return 0; // if no duplicates
     }
 };
