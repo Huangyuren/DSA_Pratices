8754a6f253b0b54a47a7b9e8e13f074d0cefcabc
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-03-05 01:30:08 +0800

    [Update] 448

--- FindAllNumbersDisappearedInArray.cpp
+++ FindAllNumbersDisappearedInArray.cpp
@@ -1,18 +1,17 @@
 // Tricky practice, just interpret absent / present information as
-// negative / positive sign on original vector~
+// negative / positive sign on original vector
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> ans;
         for(int i=0; i<nums.size(); i++){
-            int index_map = abs(nums[i]) - 1;
-            // avoid negative & negative --> positive case.
-            if(nums[index_map] > 0){
+            int index_map = abs(nums[i]) - 1; // Index corresponds to its original appeared element
+            if(nums[index_map] > 0){ // Sign can avoid duplicated elements
                 nums[index_map] *= -1;
             }
         }
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] > 0) ans.push_back(i+1);
+            if(nums[i] > 0) ans.push_back(i+1); // So now elements with positive, means absent elements
         }
         return ans;
     }
