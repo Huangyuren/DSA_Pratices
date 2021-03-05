8256aa7707cf74c658f2b8bc4402120778e75ec6
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-03-06 02:23:30 +0800

    [Update] dp starts on n

--- decodeWays.cpp
+++ decodeWays.cpp
@@ -1,92 +1,21 @@
-/* This is O(2^n), TLE. 
- * class Solution {
- * public:
- *     int recursive(string s, int start){
- *         if(s[start] == '0') return 0;
- *         if(start == s.length()) return 1;
- *         int tmp = recursive(s, start+1);
- *         if(start < s.length()-1 && (s[start]=='1' || (s[start]=='2'&&s[start+1]<'7'))){
- *             tmp += recursive(s, start+2);
- *         }
- *         return tmp;
- *     }
- *     int numDecodings(string s) {
- *         if(s == "0") return 0;
- *         return recursive(s, 0);
- *     }
- * }; */
-/* something better than above, non-dp using memoization.
- * class Solution {
- * public:
- *     int recur(string s, int start, vector<int>& vec){
- *         if(s[start] == '0') return 0;
- *         if(vec[start] > -1) {
- *             return vec[start];
- *         }
- *         int tmp = recur(s, start+1, vec);
- *         if(start < s.length()-1 && (s[start]=='1' || (s[start]=='2'&&s[start+1]<'7'))){
- *             tmp += recur(s, start+2, vec);
- *         }
- *         vec[start] = tmp;
- *         return tmp;
- *     }
- *     int numDecodings(string s) {
- *         if(s == "0") return 0;
- *         vector<int> vec(s.length()+1, -1);
- *         vec[s.length()] = 1;
- *         return recur(s, 0, vec);
- *     }
- * }; */
-
-// Using dp
-// All the way to last element in s, then go back to starting point!
-// The key spirit is that index i is determined by i+1 & i+2
-// What left behind is indexing problem
-// Time: O(1), Space: O(n)
+// Nice and neat dp solution
 class Solution {
 public:
     int numDecodings(string s) {
-        if(s == "0") return 0;
-        int n=s.length();
-        vector<int> vec(n+1, -1);
-        vec[n] = 1;
+        if(s[0] == '0') return 0;
+        int n = s.size();
+        vector<int> dp(n+1, -1); // We will always visite s[n] no matter recursive or dp, special part
+        dp[n] = 1; // initialization
         for(int i=n-1; i>=0; i--){
-            if(s[i]=='0'){
-                vec[i] = 0;
+            if(s[i] == '0'){
+                dp[i] = 0;
+                continue;
             }
-            else{
-                vec[i] = vec[i+1];
-                if(i < n-1 && (s[i]=='1' || (s[i]=='2'&&s[i+1]<'7'))){
-                    vec[i] += vec[i+2];
-                }
+            dp[i] = dp[i+1]; // previous case, regards as s[pos+1], basecase
+            if(i < n-1 && (s[i]=='1' || (s[i]=='2'&&s[i+1]<'7'))){
+                dp[i] += dp[i+2];
             }
         }
-        return vec[0];
+        return dp[0];
     }
 };
-
-// Simplified dp
-// Time: O(1), space: O(1)!, need careful thinking.
-class Solution {
-public:
-    int numDecodings(string s) {
-        if(s == "0") return 0;
-        int n=s.length();
-        int cur, prev=1, prevp;
-        for(int i=n-1; i>=0; i--){
-            if(s[i]=='0'){
-                cur = 0;
-            }
-            else{
-                cur = prev;
-                if(i < n-1 && (s[i]=='1' || (s[i]=='2'&&s[i+1]<'7'))){
-                    cur += prevp;
-                }
-            }
-            prevp = prev;
-            prev = cur;
-        }
-        return cur;
-    }
-};
-
