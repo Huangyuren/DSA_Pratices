550013a2f662af8aefa9b6f49ee1906885791ded
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-03-08 16:33:32 +0800

    [Add] 7, overflow handling

---
+++ ReverseInteger.cpp
@@ -0,0 +1,16 @@
+// Overflow correction: distangle res's formulation
+class Solution {
+public:
+    int reverse(int x) {
+        if(x==0) return 0;
+        int sign = x>0 ? 1 : -1;
+        x = abs(x);
+        int res = 0;
+        while(x > 0){
+            if(INT_MAX / 10 < res || (INT_MAX-x%10) < res*10) return 0; // 2 conditions according to next line of code
+            res = res * 10 + x%10; // Firstly cleanup one digit room for x%10, then addup together
+            x = x/10;
+        }
+        return res * sign;
+    }
+};
