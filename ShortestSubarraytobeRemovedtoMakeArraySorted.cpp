df9d6c473b40739d6bd1629a1a34b1dff103c7bd
Yu-Jen Huang <37345266+Huangyuren@users.noreply.github.com>
2021-04-04 16:13:36 +0800

    [1574] left & right pointer

---
+++ ShortestSubarraytobeRemovedtoMakeArraySorted.cpp
@@ -0,0 +1,17 @@
+// Many index tricks, start from right part for res convenience
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr) {
+        int n = arr.size();
+        int r = n-1;
+        for(; r>0 && arr[r-1] <= arr[r]; r--) ; // start from right is good
+        int res = r; // now right pointer should just be the initial res
+        for(int l=0; l<r && (l==0 || arr[l-1]<=arr[l]); l++){
+            while(r<n && arr[r] < arr[l]){
+                r++;
+            }
+            res = min(res, r-l-1);
+        }
+        return res;
+    }
+};
